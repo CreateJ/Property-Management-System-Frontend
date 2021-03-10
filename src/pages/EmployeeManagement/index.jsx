e2dbab01262b4ba2ebf79e9b1ec585e21c514389
王驰捷 <905260054@qq.com>
2021-03-10 15:25:10 +0800

    创建项目

---
+++ src/pages/EmployeeManagement/index.jsx
@@ -0,0 +1,7 @@
+const EmployeeManagement = () => {
+  return (
+    <h1>This is EmployeeManagement!</h1>
+  );
+};
+
+export default EmployeeManagement;
---
+++ src/pages/HouseholdsManagement/index.jsx
@@ -0,0 +1,6 @@
+const HouseholdsManagement = () => {
+  return (
+    <h1>This is HouseholdsManagement!</h1>
+  )
+}
+export default HouseholdsManagement
